cbd1896fb1ef9c9dba8f8a0ca6154ab8b1f3c11c
abeblanco <abro.blanco@gmail.com>
2017-05-04 00:14:13 -0500

    Maintianance

--- Blanco_Abraham_Gauss_Seidel_Meth.m
+++ Blanco_Abraham_Gauss_Seidel_Meth.m
@@ -19,30 +19,22 @@ yvalues = linspace(0,2*pi,N+2);
 %%
 %U matrix ( initial guess)
 U = ones(M+2,N+2);
-%H1= ones(M+2,N+2);
 %solving for right hand side (F equation)
 for i=1:length(xvalues);
     for j=1:length(yvalues);
 F(i,j) = cos ( (0.5*pi)* (2*((xvalues(i)-ax) / (bx - ax))+1 )).*sin( pi*((yvalues(j)-ay) / (by -ay)));
-%H(i,j) = 0; %last simulation with F=0
+%F(i,j)= 0; %last simulation with F=0
     end
 end
-
-%% Boundary Conditions for "Left" and "Right" side of Matrix
-
-% Left boundary values (Dirchelet Condition)
+%% Boundary Conditions for "Top" and "Bottom" side of Matrix
+% Top boundary values (Dirchelet Condition)
 U(1,:) = ((yvalues - ay).^2 ) .* sin( pi *(yvalues - ay) / (2*(by-ay)) ) ; 
-
-% Right boundary values (Dirchelet Condition)
+% Bottom boundary values (Dirchelet Condition)
 U(end,:) = cos (pi*(yvalues-ay)).*cosh(by-yvalues);
-
 % place these known values in the solution grid
 %solveing for Left and Right Boundaries 
 W(1,:)=U(1,:);
 W(end,:)=U(end,:);
-
-%H1(1,:) = phi_ab;
-%H1(end,:) = psy_ab;
 %%
 DX = 2*pi/(M+1);
 A = 1/DX.^2;
@@ -54,8 +46,6 @@ R = -2*(A+B);
 A = A/R;
 B = B/R;
 F = F/R;
-%H = H/R;
-
 R = 1;
 error=10;
 error_iterations=0;
@@ -70,27 +60,22 @@ for j = 2:M+1;
     % Left boundary
     W(j,1) = U(j,1);
     U(j,1) = (  F(j,1) - (2*B)*U(j,2) - A*U(j-1,1) - A*U(j+1,1) );
-    %H1(j,1) = (  H(j,1) - (2*B)*H1(j,2) - A*H1(j-1,1) - A*H1(j+1,1) );
     error(j,1) = abs((U(j,1) - W(j,1)) / U(j,1));
     % Right Boundary
     W(j,end)= U(j,end);
     U(j,end) = (  F(j,end) - (2*B)*U(j,end-1) - A*U(j-1,end) - A*U(j+1,end) );
-    %H1(j,end) = (  H(j,end) - (2*B)*H1(j,end-1) - A*H1(j-1,end) - A*H1(j+1,end) );
     error(j,M+2) = abs((U(j,M+2) - W(j,M+2)) / U(j,M+2));
 end
 %% Main Sweep of Gauss-Siedel
-
 for j= 2:M+1;
     for k = 2:N+1;
         W(j,k)=U(j,k);
         U(j,k) = (  F(j,k) - B*U(j,k-1) - B*U(j,k+1)- A*U(j-1,k) - A*U(j+1,k) );
-       % U(j,k) = lamda*U(j,k)+(1-lamda)*W(j,k);
-       % H1(j,k) = (  H(j,k) - B*H1(j,k-1) - B*H1(j,k+1)- A*H1(j-1,k) - A*H1(j+1,k) );
+       %U(j,k) = lamda*U(j,k)+(1-lamda)*W(j,k); %for SOR portion of Gauss_siedel
         error(j,k)= abs((U(j,k) - W(j,k)) / U(j,k));
     end
 end
 error=abs(max(max(((W-U)./W))));
-%error2=abs(max(max(((W2-H1)./W2))));
 error_iterations=error_iterations+1;
 end
 toc
@@ -98,6 +83,3 @@ error_iterations
 figure
 subplot(1,2,1),surf(U), xlabel('Y axis'), ylabel('X axis'), zlabel('Z axis'), title('F=cosx*siny')
 subplot(1,2,2),contour(U), xlabel('Y axis'), ylabel('X axis'), title('F=cosx*siny')
-%figure
-%subplot(1,2,1),surf(H1), xlabel('Y axis'), ylabel('X axis'), zlabel('Z axis'), title('F=0')
-%subplot(1,2,2),contour(H1), xlabel('Y axis'), ylabel('X axis'), title('F=0')
